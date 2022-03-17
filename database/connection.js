53691b2f8d07059073be40a6f9fa2208f2825d5c
shebsjose <shjosebestpeers.com>
2022-03-17 16:11:25 +0530

    connectivity to database

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/AuthTask", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to DataBase.");
+  })
+  .catch((e) => {
+    console.log("Connection Failed");
+  });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+require("./database/connection");
+
 app.listen(5000, () => {
   console.log("Listening port on 5000");
 });
