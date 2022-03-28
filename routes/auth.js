5a79a63461f40e7b73815f63be6c9782db63d5df
shebsjose <shjosebestpeers.com>
2022-03-28 11:11:16 +0530

    Create the Token for login page.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 const User = require("../models/userRegister");
 const { registerValidation, loginValidation } = require("../validation");
 
@@ -29,19 +30,22 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   console.log("Login => ",req.body);
 
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = await User.findOne(req.body.email );
+  const user = await User.findOne({email: req.body.email });
   if (!user)
     return res.status(400).send("Email or password is incorrect" );
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword)
-    return res.status(400).send("Password is incorrect" );
+  if (!validPassword) return res.status(400).send("Password is incorrect" );
+
+  const token = jwt.sign({_id : user._id}, "My_secret_token");
+  console.log(token);
+  res.header('auth-token').send(token);
 });
 
 module.exports = router;
