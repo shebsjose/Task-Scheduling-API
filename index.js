b9603757e1e88f000f1b67b4d3678196fcad5692
Sheba Jose <shjose@bestpeers.com>
2022-04-04 16:19:58 +0530

    Add routing for task

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 require("./database/connection");
 
 const authRoute = require("./routes/auth");
+const taskRoute = require("./routes/task");
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/user', authRoute);   // parent child-authroute
+app.use('/api/user', authRoute);   // parent child-authRoute
+app.use('/api/task', taskRoute);
 
 app.listen(5000, () => {
   console.log("Listening port on 5000");
