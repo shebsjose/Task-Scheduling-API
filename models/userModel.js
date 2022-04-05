0e6e04c3a201eb85bbab82f83b1e67f32bba54d5
Sheba Jose <shjose@bestpeers.com>
2022-04-05 11:05:42 +0530

    Change to Register to User Model

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+      userName: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 3,
+      },
+      email: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6,
+      },
+      password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6,
+      },
+});
+module.exports = mongoose.model("User", userSchema);
