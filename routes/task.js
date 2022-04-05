0372c498fad92383b5b0d95cd2638fa86d7e32dd
Sheba Jose <shjose@bestpeers.com>
2022-04-05 11:10:18 +0530

    Create the Route for task API

---
+++ routes/task.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = new express.Router();
+const Task= require("../models/taskModel");
+const { taskValidation } = require("../validation");
+
+router.post("/create", async (req, res) => {
+  console.log("Create Task=>", req.body);
+  const { error } = taskValidation(req.body);
+  if (error) return res.status(400).send(error);
+
+  const task = new Task({
+    description: req.body.description,
+    user : req.body.user
+  });
+  try {
+    const savedTask = await task.save();
+    res.send(savedTask);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+    console.log("get Task=>", req.body);
+    const { error } = taskValidation(req.body);
+    if (error) return res.status(400).send(error);
+  
+    try {
+        const userExists = await Task.findOne({_id: req.params.id});
+        res.send(userExists);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
+
+  module.exports = router;
+
