c4bc58dcc8344332c6d309060d316e87413832bc
Sheba Jose <shjose@bestpeers.com>
2022-05-03 18:45:42 +0530

    Add time field

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
     description: {
@@ -13,5 +14,8 @@ const taskSchema = new mongoose.Schema({
         type: String,
         default: 'requested'
       },
+      time :{
+        type : String
+      }
 }, { timestamps: true});
 module.exports = mongoose.model("Task", taskSchema); 
