191a4051da842a055055d55a6561330943f0390d
Sheba Jose <shjose@bestpeers.com>
2022-05-11 12:33:36 +0530

    Changes in validation

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const registerValidation = (data) => {
     const schema = Joi.object({
-      userName: Joi.string().min(3).required(),
+      userName: Joi.string().min(3).max(8).required(),
       email: Joi.string().min(6).required().email(),
       password: Joi.string().min(6).required(),
     });
